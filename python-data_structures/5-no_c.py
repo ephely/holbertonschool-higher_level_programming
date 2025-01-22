96eed1da9a8dcc36b5828732f398e1a416df33f1
« Ephely » <ephely1@gmail.com>
2025-01-22 14:19:43 +0100

    Python

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    chars = list(my_string)
-    for char in chars:
-        if char == 'c' or char == 'C':
-            chars.remove(char)
-    return ("".join(chars))
+    return "".join(char for char in my_string if char not in ('c', 'C'))
