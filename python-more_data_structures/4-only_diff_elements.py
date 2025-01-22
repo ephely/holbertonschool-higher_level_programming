f7125ac91925a65289f8e82f735a8a8da5c76bc0
« Ephely » <ephely1@gmail.com>
2025-01-22 16:53:51 +0100

    diff

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    symmetric_diff = ((set_1) - (set_2))
-    print(symmetric_diff)
+    symmetric_diff = ((set_1) ^ (set_2))
+    return (symmetric_diff)
