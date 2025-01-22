8bbe122532b7c6a4878b1141d015b42b27db8077
« Ephely » <ephely1@gmail.com>
2025-01-22 14:06:43 +0100

    Python

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    chars = list(my_string)
+    for char in chars:
+        if char == 'c' or char == 'C':
+            chars.remove(char)
+    return("".join(chars))
