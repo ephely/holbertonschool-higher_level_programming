9f00e79b6977ea3d86de1671d4432a36b5ad83c9
« Ephely » <ephely1@gmail.com>
2025-01-24 14:07:16 +0100

    div

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         div = (a / b)
     except (ZeroDivisionError):
-            div = None
+        div = None
     finally:
         print("Inside result: {}".format(div))
     return (div)
