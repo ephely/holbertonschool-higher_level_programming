38419793456696e6329e865234b78501cf04ac38
« Ephely » <ephely1@gmail.com>
2025-01-26 10:53:02 +0100

    div

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     final = []
-    for i in range(0, list_length-1):
+    for i in range(0, list_length):
         try:
             div = my_list_1[i]/my_list_2[i]
             final.append(div)
-            pass
         except TypeError:
             print("wrong type")
             final.append(0)
-            pass
         except ZeroDivisionError:
             print("division by 0")
             final.append(0)
-            pass
         except IndexError:
             print("out of range")
             final.append(0)
+        finally:
             pass
+        final.append(div)
     return (final)
