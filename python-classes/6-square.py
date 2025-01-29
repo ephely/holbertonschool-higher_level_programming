2bc875b732c7367c9bda236b3ee2765b9d417142
« Ephely » <ephely1@gmail.com>
2025-01-29 23:47:44 +0100

    6 square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -49,10 +49,11 @@ class Square:
 
     def my_print(self):
         """Prints in stdout the square with the character #."""
+        if self.__size == 0:
+            print("")
+            
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
-            print("")
-        if self.__size == 0:
             print("")
