089946df2cd5b23057d459d4f7cfae21acb0b498
« Ephely » <ephely1@gmail.com>
2025-01-29 11:23:06 +0100

    self

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size : Size of the square.
+        """
+        self.__size = size
+
+    def size(self):
+        return self.__size
+    
+    def size(self, value):
+          """Handle the errors."""
+          if not isinstance(value, int):
+               raise TypeError("size must be an integer")
+          elif value < 0:
+              raise ValueError("size must be >= 0")
+          self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
