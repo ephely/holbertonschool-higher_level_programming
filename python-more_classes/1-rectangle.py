7060c808c7e189149c2b6458765cfc7dd82b4771
« Ephely » <ephely1@gmail.com>
2025-01-30 09:58:14 +0100

    1 rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,14 +5,14 @@
 class Rectangle:
     """Represent a rectangle."""
     def __init__(self, width=0, height=0):
-        """Initialize a new Square.
+        """Initialize a new Rectangle.
 
         Args:
             width : width of the rectangle.
             height : height of the rectangle.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
