8948744ae3fa215b9a3d629fbd544c4a4bc47d01
« Ephely » <ephely1@gmail.com>
2025-02-05 00:23:23 +0100

    2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,17 +2,15 @@
 """Defines an inherited list class MyList."""
 
 
-class MyList(list):
-    """Inherits from list."""
-
-    def is_same_class(obj, a_class):
-        """returns True if the object is
+def is_same_class(obj, a_class):
+    """returns True if the object is
         exactly an instance of the specified class.
-
-        Args:
+        
+        
+    Args:
         obj: The object to check.
         a_class: The class to match the type of obj against.
-        """
-        if type(obj) == a_class:
-            return True
-        return False
+    """
+    if type(obj) == a_class:
+        return True
+    return False
