12237f5e2a6c7cbf814f93a5a0f49dd6cee744cf
« Ephely » <ephely1@gmail.com>
2025-02-06 03:17:00 +0100

    base geometry

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Raises an exception if the method is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validator for integers.
+
+        Args:
+            name (str): The name of the value.
+            value (int): The value to validate.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
