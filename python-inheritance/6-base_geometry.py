c7795c472159c211033903c49fae2e67f5a1a0f9
« Ephely » <ephely1@gmail.com>
2025-02-06 11:06:27 +0100

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Raises an exception if the method is not implemented."""
+        raise Exception("area() is not implemented")
