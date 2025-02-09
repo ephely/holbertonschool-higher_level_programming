6bdf150897d7f5082d5c7012f48588b7d00ed33c
« Ephely » <ephely1@gmail.com>
2025-02-09 15:12:49 +0100

    7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
             name (str): The name of the value.
             value (int): The value to validate.
         """
-        if type(value) != int:
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
