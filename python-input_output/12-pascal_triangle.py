0f741d67f51027d77b280b4ba50e98a7ae746ca1
« Ephely » <ephely1@gmail.com>
2025-02-15 22:21:49 +0100

    Added the docstrings

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Returns a list of lists of integers representing the Pascal’s triangle of n."""
+""" 
+Returns a list of lists of integers 
+representing the Pascal’s triangle of n.
+"""
 
 
 def pascal_triangle(n):
