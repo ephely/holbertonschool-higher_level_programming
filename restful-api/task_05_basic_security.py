1e6d850404731c3beb9f899979afc617d06ce0b3
« Ephely » <ephely1@gmail.com>
2025-02-21 22:36:56 +0100

    files pushed

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,10 +1,10 @@
+"""API Security and Authentication Techniques"""
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import (
     JWTManager, create_access_token, jwt_required, get_jwt_identity
 )
 from werkzeug.security import generate_password_hash, check_password_hash
-"""API Security and Authentication Techniques"""
 
 
 app = Flask(__name__)
